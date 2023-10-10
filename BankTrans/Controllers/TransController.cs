8a1439a8eda93f2e736853febe2f946e187a7921
Hasan Shahariar <hasanshahariar550@gmail.com>
2023-10-10 15:05:08 +0600

    authorize send api

--- BankTrans/Controllers/TransController.cs
+++ BankTrans/Controllers/TransController.cs
@@ -24,7 +24,7 @@ namespace BankTrans.Controllers
             _mapper = mapper;
         }
         [HttpPost]
-
+        [Authorize]
         public async Task<IActionResult> SaveTransData(CityBankTransactionCreateDto model)
         {
             CityBankTransaction cityBankTransaction = new CityBankTransaction();
